1e18b0f14821d18d5651fa6ec0efcb2b2e7d5f92
AIiRcz1115 <200611@student.pwr.wroc.pl>
2016-04-09 02:52:08 +0200

    Let's begin
    prosta aplikacja wyszukująca urządzenia BT w zasięgu. Layouty mnie
    pokonały i nie udało mi się ustawić koloru tekstu na czarny przez co
    jest mała widoczność wyszukanych elementów, ale działa

---
+++ MobileApp/ESignboard/app/src/main/java/com/example/r2d2/esignboard/MainActivity.java
@@ -0,0 +1,79 @@
+package com.example.r2d2.esignboard;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.Color;
+import android.os.Bundle;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.example.bluetoothexample.R;
+import java.util.ArrayList;
+
+
+public class MainActivity extends Activity {
+
+    private BluetoothAdapter mBtAdapter;
+    private ListView mLvDevices;
+    private ArrayList<String> mDeviceList = new ArrayList<String>();
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        mLvDevices = (ListView) findViewById(R.id.lvDevices);
+
+
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(mBtReceiver, filter);
+
+        // Getting the Bluetooth adapter
+        mBtAdapter = BluetoothAdapter.getDefaultAdapter();
+        if(mBtAdapter != null) {
+            mBtAdapter.startDiscovery();
+            Toast.makeText(this, "Starting discovery...", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Toast.makeText(this, "Bluetooth disabled or not available.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mBtAdapter != null) {
+            mBtAdapter.cancelDiscovery();
+        }
+        unregisterReceiver(mBtReceiver);
+    }
+
+    private final BroadcastReceiver mBtReceiver = new BroadcastReceiver() {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                mDeviceList.add(device.getAddress() + ", " + device.getName()); // get mac address
+
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, mDeviceList);
+                mLvDevices.setAdapter(adapter);
+            }
+        }
+    };
+
+
+}
