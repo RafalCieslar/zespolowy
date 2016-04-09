b5d738bb105d88332c885b5d7cf4a91799c82dc1
AIiRcz1115 <200691@student.pwr.wroc.pl>
2016-04-09 20:33:15 +0200

    Trying to add BT connectivity

--- MobileApp/ESignboard/app/src/main/java/com/example/r2d2/esignboard/MainActivity.java
+++ MobileApp/ESignboard/app/src/main/java/com/example/r2d2/esignboard/MainActivity.java
@@ -8,19 +8,27 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import com.example.bluetoothexample.R;
 
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 
 public class MainActivity extends Activity {
 
+    // mBtAdapter to moduł BT w tym urządzeniu
     private BluetoothAdapter mBtAdapter;
+    // mLvDevices to wyświetlana lista w aplikacji
     private ListView mLvDevices;
-    private ArrayList<String> mDeviceList = new ArrayList<String>();
+    // mStringDeviceList to elementy wyświetlanej listy
+    private ArrayList<String> mStringDeviceList = new ArrayList<String>();
+    // mBtDeviceList to lista znalezionych odbiorników BT
+    private ArrayList<BluetoothDevice> mBtDeviceList = new ArrayList<BluetoothDevice>();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -32,21 +40,48 @@ public class MainActivity extends Activity {
         IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
         registerReceiver(mBtReceiver, filter);
 
+
         // Getting the Bluetooth adapter
         mBtAdapter = BluetoothAdapter.getDefaultAdapter();
+        // Gdy urządzenie ma moduł BT
         if(mBtAdapter != null) {
-            mBtAdapter.startDiscovery();
-            Toast.makeText(this, "Starting discovery...", Toast.LENGTH_SHORT).show();
+            //Gdy moduł BT jest włączony
+            if(mBtAdapter.isEnabled()) {
+                // Rozpoczęcie wyszukiwania urządzeń BT
+                mBtAdapter.startDiscovery();
+                Toast.makeText(this, getResources().getString(R.string.BT_start_discovery), Toast.LENGTH_SHORT).show();
+            }
+            else {
+                Toast.makeText(this, getResources().getString(R.string.BT_disabled), Toast.LENGTH_SHORT).show();
+                //Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                //startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            }
         }
+        // Gdy urządzenie nie ma modułu BT
         else {
-            Toast.makeText(this, "Bluetooth disabled or not available.", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, getResources().getString(R.string.BT_no_adapter), Toast.LENGTH_SHORT).show();
         }
+
+        // Gdy wybrano któryś element z wyświetlanej listy
+        mLvDevices.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Zatrzymanie wyszukiwania urządzeń BT
+                if (mBtAdapter.isDiscovering()) {
+                    mBtAdapter.cancelDiscovery();
+                }
+                // Wybranie odpowiedniego urządzenia BT
+                BluetoothDevice device = mBtDeviceList.get(position);
+                // Funkcja łączoca z odpowiednim urządzeniem BT
+                connect(device);
+            }
+        });
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if (mBtAdapter != null) {
+        if (mBtAdapter.isDiscovering()) {
             mBtAdapter.cancelDiscovery();
         }
         unregisterReceiver(mBtReceiver);
@@ -54,16 +89,34 @@ public class MainActivity extends Activity {
 
     private final BroadcastReceiver mBtReceiver = new BroadcastReceiver() {
 
+        // funkcja wywoływana gdy wykryto nowy odbiornik BT
         @Override
         public void onReceive(Context context, Intent intent) {
             String action = intent.getAction();
             if (BluetoothDevice.ACTION_FOUND.equals(action)) {
                 BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                mDeviceList.add(device.getAddress() + ", " + device.getAddress()); // get mac address
-
-                ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, mDeviceList);
+                // Dodanie znalezionego urządzenia do wyświetlanej listy
+                mStringDeviceList.add(device.getAddress() + ", " + device.getAddress());
+                // Dodanie znalezionego urządzenia do listy urządzeń
+                mBtDeviceList.add(device);
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, mStringDeviceList);
                 mLvDevices.setAdapter(adapter);
             }
         }
+    };
+
+    // Funkcja łącząca z odpowiednim urządzeniem BT
+    private Boolean connect(BluetoothDevice bdDevice) {
+        Boolean bool = false;
+        try {
+            Class cl = Class.forName("android.bluetooth.BluetoothDevice");
+            Class[] par = {};
+            Method method = cl.getMethod("createBond", par);
+            Object[] args = {};
+            bool = (Boolean) method.invoke(bdDevice);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return bool.booleanValue();
     };
 }
