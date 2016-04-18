5d9b25d0257079529239b6a97b7411cf4d510955
Kangel <k4ngel@gmail.com>
2016-04-18 05:38:56 +0200

    Added app name

--- WebApp/ESignboard/user/urls.py
+++ WebApp/ESignboard/user/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 
 from . import views
 
+app_name = 'user'
 urlpatterns = [
-    url(r'^login$', views.login, name='login'),
-    url(r'^register$', views.register, name='register'),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^register/$', views.register, name='register'),
 ]
