0ad470c86a60dbbcc3876f5efe87412943e87933
Kangel <k4ngel@gmail.com>
2016-04-18 05:39:50 +0200

    Added actual registering and logging in procedures

--- WebApp/ESignboard/user/views.py
+++ WebApp/ESignboard/user/views.py
@@ -1,11 +1,49 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import authenticate, login as auth_login
 # Create your views here.
 
 
 def login(request):
-    return render(request, 'user/login.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                auth_login(request, user)
+                message = 'Zalogowano'
+            else:
+                message = 'Użytkownik nieaktywny'
+        else:
+            message = 'Błędne dane logowania'
+    else:
+        message = ''
+
+    return render(request, 'user/login.html', {
+        'form': AuthenticationForm,
+        'message': message,
+    })
 
 
 def register(request):
-    return render(request, 'user/register.html')
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = User.objects.create_user(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password1'],
+            )
+            message = 'Stworzono użytkownika'
+        else:
+            message = 'Błędnie wypełniony formularz'
+    else:
+        form = UserCreationForm()
+        message = ''
+
+    #form = RegisterForm()
+    return render(request, 'user/register.html', {
+        'form': form,
+        'message': message,
+    })
