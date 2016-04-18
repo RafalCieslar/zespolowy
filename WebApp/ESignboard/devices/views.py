50f4ffd81aafb8d0c1edb955575bdc14b62508ab
Kangel <k4ngel@gmail.com>
2016-04-18 05:37:00 +0200

    Restricted view only for logged users

--- WebApp/ESignboard/devices/views.py
+++ WebApp/ESignboard/devices/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.urlresolvers import reverse
 
 # Create your views here.
 
 
 def dashboard(request):
+    if not request.user.is_authenticated():
+        return redirect('user:login')
     return render(request, 'devices/dashboard.html')
