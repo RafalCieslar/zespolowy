f356c92731320c27a69ffe41000e9394c2b0dbba
Kangel <k4ngel@gmail.com>
2016-04-18 04:51:49 +0200

    Changed language and timezone

--- WebApp/ESignboard/ESignboard/settings.py
+++ WebApp/ESignboard/ESignboard/settings.py
@@ -107,9 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pl'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'CET'
 
 USE_I18N = True
 
