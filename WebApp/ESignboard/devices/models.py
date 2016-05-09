6196f85ea91c7fbe5a237c2656d8b279dd6414a2
Kangel <k4ngel@gmail.com>
2016-05-09 09:54:46 +0200

    Changed relations to many to many, added some fields

--- WebApp/ESignboard/devices/models.py
+++ WebApp/ESignboard/devices/models.py
@@ -1,19 +1,30 @@
 from django.db import models
 from django.contrib.auth.models import User
+
 # Create your models here.
+
+
 class Device(models.Model):
-    location = models.CharField(max_length=30)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    owners = models.ManyToManyField(User)
+    hash = models.CharField(max_length=32)
+
     def __str__(self):
-        return self.location
+        return self.name
+
     def compile_cache(self):
         #to do: cache compilation
         return 1
 
+
 class Poi(models.Model):
     name = models.CharField(max_length=30)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    owners = models.ManyToManyField(User)
     parent_device = models.ForeignKey(Device, on_delete=models.CASCADE)
     content = models.FileField(upload_to='files') #+parent_device_id+'/'+id
+    uuid = models.CharField(max_length=32)
+
     def __str__(self):
         return self.name
