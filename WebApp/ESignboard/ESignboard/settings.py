682d1fca456a0fabbfb90dd7bbab59d0a28d4e94
jediflamaster <jediflamaster.kg@gmail.com>
2016-05-09 08:18:42 +0200

    Added device&POI database models

--- WebApp/ESignboard/ESignboard/settings.py
+++ WebApp/ESignboard/ESignboard/settings.py
@@ -37,9 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main',
-    'user',
-    'devices'
+    'main.apps.MainConfig',
+    'user.apps.UserConfig',
+    'devices.apps.DevicesConfig'
 ]
 
 MIDDLEWARE_CLASSES = [
