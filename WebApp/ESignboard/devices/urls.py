c974bd2d2172cd4732058cd0b1b3614168a89bbc
Kangel <k4ngel@gmail.com>
2016-05-09 09:54:58 +0200

    Added urls

--- WebApp/ESignboard/devices/urls.py
+++ WebApp/ESignboard/devices/urls.py
@@ -5,4 +5,10 @@ from . import views
 app_name = 'devices'
 urlpatterns = [
     url(r'^dashboard/$', views.dashboard, name='dashboard'),
+    url(r'^update/(?P<device_id>[0-9a-z]+)$', views.update, name='update'),
+    url(r'^download/(?P<device_id>[0-9a-z]+)$', views.download, name='download'),
+    url(r'^poi/view/(?P<device_id>[0-9a-z]+)$', views.poi_view, name='poi_view'),
+    url(r'^poi/edit/(?P<device_id>[0-9a-z]+)$', views.poi_edit, name='poi_edit'),
+    url(r'^mdevice/view/(?P<device_id>[0-9]+)$', views.mdevice_view, name='mdevice_view'),
+    url(r'^mdevice/edit/(?P<device_id>[0-9]+)$', views.mdevice_edit, name='mdevice_edit')
 ]
