05035c93cc4e3ef6f4d406c513d5cd7f593b81da
Kangel <k4ngel@gmail.com>
2016-05-09 09:26:05 +0200

    Deleted custom forms for register and login

--- WebApp/ESignboard/user/forms.py
+++ WebApp/ESignboard/user/forms.py
@@ -1,15 +1,15 @@
 from django import forms
 
 
-class RegisterForm(forms.Form):
-    username = forms.CharField(label="Nazwa użytkownika", required=True, max_length=30)
-    first_name = forms.CharField(label="Imię", required=True, max_length=30)
-    last_name = forms.CharField(label="Nazwisko", required=True, max_length=30)
-    password = forms.CharField(label="Hasło:", required=True, widget=forms.PasswordInput())
-    confirm_password = forms.CharField(label="Powtórz hasło:", required=True, widget=forms.PasswordInput())
-    email = forms.EmailField(label="Adres e-mail", required=True)
-
-
-class LoginForm(forms.Form):
-    username = forms.CharField(label="Nazwa użytkownika", required=True, max_length=30)
-    password = forms.CharField(label="Hasło:", required=True, widget=forms.PasswordInput())
+# class RegisterForm(forms.Form):
+#     username = forms.CharField(label="Nazwa użytkownika", required=True, max_length=30)
+#     first_name = forms.CharField(label="Imię", required=True, max_length=30)
+#     last_name = forms.CharField(label="Nazwisko", required=True, max_length=30)
+#     password = forms.CharField(label="Hasło:", required=True, widget=forms.PasswordInput())
+#     confirm_password = forms.CharField(label="Powtórz hasło:", required=True, widget=forms.PasswordInput())
+#     email = forms.EmailField(label="Adres e-mail", required=True)
+#
+#
+# class LoginForm(forms.Form):
+#     username = forms.CharField(label="Nazwa użytkownika", required=True, max_length=30)
+#     password = forms.CharField(label="Hasło:", required=True, widget=forms.PasswordInput())
