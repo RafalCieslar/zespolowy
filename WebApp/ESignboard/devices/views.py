fa71400e7bc3bd7cc889fa747ef62aa705e8105a
Kangel <k4ngel@gmail.com>
2016-05-22 19:00:02 +0200

    Check if files exist

--- WebApp/ESignboard/devices/views.py
+++ WebApp/ESignboard/devices/views.py
@@ -2,19 +2,17 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.http import Http404, JsonResponse, HttpResponse
 import zipfile
 from hashlib import md5
-
+import os
 from .models import Device, Poi
-from django.core.urlresolvers import reverse
-
-# tego zdecydowanie tutaj nie powinno być
 
 
-def generate_md5(fname):
+def generate_md5(fname):  # tego zdecydowanie tutaj nie powinno być
     hash_md5 = md5()
     with open(fname, "rb") as f:
         for chunk in iter(lambda: f.read(4096), b""):
             hash_md5.update(chunk)
     return hash_md5.hexdigest()
+
 
 # Create your views here.
 
@@ -30,10 +28,17 @@ def dashboard(request):
 def update(request, device_id):
     mdevice = get_object_or_404(Device, pk=device_id)
     pois = Poi.objects.filter(parent_device__in=[mdevice.id])
-    zip_file = zipfile.ZipFile('files/' + str(mdevice.id) + '/update.zip', mode='w')
+    files_dir = 'files/' + str(mdevice.id) + '/'
+    zip_filename = 'update.zip'  # without trailing slash
+    html_filename = '/index.html'  # with trailing slash
+    img_filename = '/file.jpg'  # with trailing slash
+
+    zip_file = zipfile.ZipFile(files_dir + zip_filename, mode='w')
     for poi in pois:
-        zip_file.write('files/' + str(mdevice.id) + '/' + poi.uuid + '/index.html', poi.uuid + '/index.html')
-        zip_file.write('files/' + str(mdevice.id) + '/' + poi.uuid + '/file.jpg', poi.uuid + '/file.jpg')
+        if os.path.isfile(files_dir + poi.uuid + html_filename):
+            zip_file.write(files_dir + poi.uuid + html_filename, poi.uuid + html_filename)
+        if os.path.isfile(files_dir + poi.uuid + html_filename):
+            zip_file.write(files_dir + poi.uuid + img_filename, poi.uuid + img_filename)
     zip_file.close()
 
     mdevice.hash = generate_md5('files/' + str(mdevice.id) + '/update.zip')
@@ -44,7 +49,7 @@ def update(request, device_id):
 
 def download(request, device_id):
     mdevice = get_object_or_404(Device, pk=device_id)
-    zip_file = open('files/'+str(mdevice.id)+'/update.zip', 'rb')
+    zip_file = open('files/' + str(mdevice.id) + '/update.zip', 'rb')
     response = HttpResponse(zip_file, content_type='application/zip')
     response['Content-Disposition'] = 'attachment; filename="%s"' % 'update.zip'
     return response
@@ -54,7 +59,7 @@ def poi_view(request, device_id):
     if not request.user.is_authenticated():
         return redirect('user:login')
     device = get_object_or_404(Poi, pk=device_id)
-    if not device.objects.filter(owners__in=request.user.id).exists()\
+    if not device.objects.filter(owners__in=request.user.id).exists() \
             and device.mdevice.objects.filter(owner__in=request.user.id).exists():
         return Http404
     return render(request, 'devices/view.html')
@@ -64,7 +69,7 @@ def poi_edit(request, device_id):
     if not request.user.is_authenticated():
         return redirect('user:login')
     device = get_object_or_404(Poi, pk=device_id)
-    if not device.objects.filter(owners__in=request.user.id).exists()\
+    if not device.objects.filter(owners__in=request.user.id).exists() \
             and device.mdevice.objects.filter(owners__in=request.user.id).exists():
         return Http404
     return render(request, 'devices/edit.html')
@@ -86,4 +91,3 @@ def mdevice_edit(request, device_id):
     if not device.objects.filter(owners__in=request.user.id).exists():
         return Http404
     return render(request, 'devices/edit.html')
-
