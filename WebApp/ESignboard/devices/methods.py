122db70bcdf001a92b1a3eccc35c84382d2c2aa3
Kangel <k4ngel@gmail.com>
2016-05-28 02:45:09 +0200

    Zip apparently works even without the single file hack

--- WebApp/ESignboard/devices/methods.py
+++ WebApp/ESignboard/devices/methods.py
@@ -2,8 +2,10 @@ import os
 import io
 import errno
 import zipfile
+import zlib
 from .models import Poi, Device
 from hashlib import md5
+from shutil import copyfileobj
 
 
 def handlefile(f, path):
@@ -21,7 +23,7 @@ def checkpath(path):
             raise
 
 
-def generate_md5(fname):  # tego zdecydowanie tutaj nie powinno być
+def generate_md5(fname):
     hash_md5 = md5()
     with open(fname, "rb") as f:
         for chunk in iter(lambda: f.read(4096), b""):
@@ -36,16 +38,22 @@ def createzip(poiObject):
     zip_filename = 'update.zip'  # without trailing slash
     html_filename = '/index.html'  # with trailing slash
     img_filename = '/file.jpg'  # with trailing slash
-    temp_file = io.StringIO('readthis')
+    #temp_file = io.StringIO('readthis')
+    # zip_file_holder = io.BytesIO()
 
-    zip_file = zipfile.ZipFile(files_dir + zip_filename, mode='w')
-    zip_file.writestr(temp_file.getvalue(), 'read')
+    # zip_file = zipfile.ZipFile(zip_file_holder, mode='w', compression=zipfile.ZIP_DEFLATED)
+    zip_file = zipfile.ZipFile(files_dir + zip_filename, mode='w', compression=zipfile.ZIP_DEFLATED)
+    # zip_file.writestr('read', temp_file.getvalue())
     for poi in pois:
         if os.path.isfile(files_dir + poi.uuid + html_filename):
             zip_file.write(files_dir + poi.uuid + html_filename, poi.uuid + html_filename)
         if os.path.isfile(files_dir + poi.uuid + html_filename):
             zip_file.write(files_dir + poi.uuid + img_filename, poi.uuid + img_filename)
     zip_file.close()
+
+    # with open(files_dir + zip_filename, mode='wb') as fd:
+    #     zip_file_holder.seek(0)
+    #     copyfileobj(zip_file_holder, fd)
 
     mdevice.hash = generate_md5('files/' + str(mdevice.id) + '/update.zip')
     mdevice.save()
